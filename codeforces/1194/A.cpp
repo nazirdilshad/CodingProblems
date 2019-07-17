d8822fc8d157984e43f53636916671b0eb29a697
Nazir Dilshad <dilshadobryan@gmail.com>
2019-07-17 16:42:00 +0530

    Add solution for problem `A - Remove a Progression` Link: https://codeforces.com/contest/1194/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1194/submission/57192696

---
+++ codeforces/1194/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    
+    int T;
+    cin>>T;
+	ll n,x;
+	for(int i=0;i<T;i++){
+	cin>>n>>x;
+	cout<<2*x<<"\n";
+	}
+	return 0;
+}
