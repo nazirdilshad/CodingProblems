2a51976d0ccf41ee64ef38e925b6f9b93e46c8b7
Nazir Dilshad <dilshadobryan@gmail.com>
2020-06-07 14:03:00 +0530

    Add solution for problem `C - Johnny and Another Rating Drop` Link: https://codeforces.com/contest/1362/problem/C Tags: bitmasks, greedy, math, *1400 Ref: https://codeforces.com/contest/1362/submission/82762953

---
+++ codeforces/1362/C.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> p64;
+typedef vector<ll> v420;
+
+#define pb push_back
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define maxi 100000
+#define pii pair<int, int>
+#define eb emplace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define mem(x, y) memset(x, y, sizeof(x))
+#define DANGER                        \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll sum = 0, x = 2;
+    while (n > 0)
+    {
+        sum += n;
+        n = n / 2;
+    }
+    cout << sum << endl;
+}
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
