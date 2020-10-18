11d21e4b299a10d0c682eb45a7bb6f47584fcdfe
Nazir Dilshad <dilshadobryan@gmail.com>
2020-10-18 14:39:00 +0530

    Add solution for problem `A - XORwice` Link: https://codeforces.com/contest/1421/problem/A Tags: bitmasks, greedy, math, *800 Ref: https://codeforces.com/contest/1421/submission/95858863

---
+++ codeforces/1421/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long long int lld;
+typedef pair<ll, ll> p64;
+typedef vector<ll> v420;
+
+#define pb push_back
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define maxi 100000
+#define pii pair<int, int>
+#define eb emplace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define mem(x, y) memset(x, y, sizeof(x))
+#define DANGER                        \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+ll mod = 1e9 + 7;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << (a xor b) << endl;
+}
+
+int main()
+{
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
