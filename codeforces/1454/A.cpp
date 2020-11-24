f8af9bf263024ddf3a4d198c5e0ada68d3fd931e
Nazir Dilshad <dilshadobryan@gmail.com>
2020-11-24 20:12:00 +0530

    Add solution for problem `A - Special Permutation` Link: https://codeforces.com/contest/1454/problem/A Tags: constructive algorithms, probabilities, *800 Ref: https://codeforces.com/contest/1454/submission/99418485

---
+++ codeforces/1454/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> p64;
+typedef vector<ll> v420;
+
+#define pb push_back
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define maxi 100000
+#define pii pair<int, int>
+#define eb emplace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define mem(x, y) memset(x, y, sizeof(x))
+#define DANGER                        \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (int i = 0; i < n - 1; i++)
+    {
+        cout << i + 2 << " ";
+    }
+    cout << 1 << " ";
+    cout << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
