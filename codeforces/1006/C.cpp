808427efe8bb3def58921ac4573b5d89e6967ab8
Nazir Dilshad <dilshadobryan@gmail.com>
2020-11-30 16:51:00 +0530

    Add solution for problem `C - Three Parts of the Array` Link: https://codeforces.com/contest/1006/problem/C Tags: binary search, data structures, two pointers, *1200 Ref: https://codeforces.com/contest/1006/submission/99978615

---
+++ codeforces/1006/C.cpp
@@ -0,0 +1,131 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// typedef long long ll;
+// typedef pair<ll, ll> p64;
+// typedef vector<ll> v420;
+
+// #define pb push_back
+// #define rep(i, a, b) for (int i = a; i < b; i++)
+// #define maxi 100000
+// #define pii pair<int, int>
+// #define eb emplace_back
+// #define mp make_pair
+// #define ff first
+// #define ss second
+// #define all(x) x.begin(), x.end()
+// #define mem(x, y) memset(x, y, sizeof(x))
+// #define DANGER                        \
+//     std::ios::sync_with_stdio(false); \
+//     cin.tie(NULL);                    \
+//     cout.tie(NULL)
+
+// void solve()
+// {
+//     ll n, sum = 0;
+//     cin >> n;
+//     ll a[n], b[n], c[n];
+//     rep(i, 0, n)
+//     {
+//         cin >> a[i];
+//         sum += a[i];
+//         if (i == 0)
+//         {
+//             b[i] = a[i];
+//             continue;
+//         }
+//         b[i] = a[i] + b[i - 1];
+//     }
+
+//     c[0] = a[n - 1];
+//     for (int i = n - 2, k = 1; i >= 0; i--, k++)
+//     {
+//         c[k] = c[k - 1] + a[i];
+//     }
+//     ll ans = 0;
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         int t = lower_bound(c, c + n, b[i]) - c;
+//         if (c[t] == b[i] )
+//         {
+//             ans = max(ans, c[t]);
+//         }
+//     }
+//     cout << ans;
+// }
+
+// int main()
+// {
+//     int t = 1;
+//     //cin >> t;
+//     while (t--)
+//     {
+//         solve();
+//     }
+// }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> p64;
+typedef vector<ll> v420;
+
+#define pb push_back
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define maxi 100000
+#define pii pair<int, int>
+#define eb emplace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(x) x.begin(), x.end()
+#define mem(x, y) memset(x, y, sizeof(x))
+#define DANGER                        \
+    std::ios::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+void solve()
+{
+    ll n, sum = 0;
+    cin >> n;
+    ll a[n], b[n], c[n];
+    rep(i, 0, n)
+    {
+        cin >> a[i];
+        sum += a[i];
+        if (i == 0)
+        {
+            b[i] = a[i];
+            continue;
+        }
+        b[i] = a[i] + b[i - 1];
+    }
+    c[n - 1] = a[n - 1];
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        c[i] = c[i + 1] + a[i];
+    }
+    ll ans = 0;
+    for (int i = n - 1; i > 0; i--)
+    {
+        int idx = lower_bound(b, b + n - 1, c[i]) - b;
+        if (idx < i && b[idx] == c[i])
+        {
+            ans = max(ans, b[idx]);
+        }
+    }
+    cout << ans;
+}
+
+int main()
+{
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
